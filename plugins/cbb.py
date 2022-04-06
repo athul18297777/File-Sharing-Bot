1dc5bd48974414bb8bce7ed9c7ddc9ba10f13ae5
athulx <88526768+athul18297777@users.noreply.github.com>
2022-04-06 19:57:36 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ 𝘾𝙧𝙚𝙖𝙩𝙤𝙧 : <a href='tg://user?id={1285659199}'>[𝘼𝙩𝙝𝙪𝙡](https://t.me/athulx80)</a>\n○ 𝙇𝙖𝙣𝙜𝙪𝙖𝙜𝙚 : <code>Python3</code>\n○ 𝙇𝙞𝙗𝙧𝙖𝙧𝙮 : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ 𝙎𝙤𝙪𝙧𝙘𝙚 𝘾𝙤𝙙𝙚 : <a href='https://github.com/athul18297777/File-Sharing-Bot'>𝘾𝙡𝙞𝙘𝙠 𝙃𝙚𝙧𝙚</a>\n○ 𝘾𝙝𝙖𝙣𝙣𝙚𝙡 : @newallmoviesx\n○ 𝙎𝙪𝙥𝙥𝙤𝙧𝙩 𝙂𝙧𝙤𝙪𝙥 : @newallmoviesxgroup</b>",
+            text = f"<b>○ 𝘾𝙧𝙚𝙖𝙩𝙤𝙧 : <a href='tg://user?id={1285659199}'[𝘼𝙩𝙝𝙪𝙡](https://t.me/athulx80)</a>\n○ 𝙇𝙖𝙣𝙜𝙪𝙖𝙜𝙚 : <code>𝙋𝙮𝙩𝙝𝙤𝙣𝟯</code>\n○ 𝙇𝙞𝙗𝙧𝙖𝙧𝙮 : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ 𝙎𝙤𝙪𝙧𝙘𝙚 𝘾𝙤𝙙𝙚 : <a href='https://github.com/athul18297777/File-Sharing-Bot'>𝘾𝙡𝙞𝙘𝙠 𝙃𝙚𝙧𝙚</a>\n○ 𝘾𝙝𝙖𝙣𝙣𝙚𝙡 : @newallmoviesx\n○ 𝙎𝙪𝙥𝙥𝙤𝙧𝙩 𝙂𝙧𝙤𝙪𝙥 : @newallmoviesxgroup</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
